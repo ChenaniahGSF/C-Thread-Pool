94a0b75edad129ce0fef63794fe4beafca292bc4
shanfge <yeyinggsf@gmail.com>
2025-03-29 00:31:41 +0800

    add example of passing struct to task function

--- example.c
+++ example.c
@@ -11,30 +11,54 @@
  * 
  * */
 
-#include <stdio.h>
-#include <pthread.h>
-#include <stdint.h>
-#include "thpool.h"
-
-void task(void *arg){
-	printf("Thread #%u working on %d\n", (int)pthread_self(), (int) arg);
-}
-
-
-int main(){
-	
-	puts("Making threadpool with 4 threads");
-	threadpool thpool = thpool_init(4);
-
-	puts("Adding 40 tasks to threadpool");
-	int i;
-	for (i=0; i<40; i++){
-		thpool_add_work(thpool, task, (void*)(uintptr_t)i);
-	};
-
-	thpool_wait(thpool);
-	puts("Killing threadpool");
-	thpool_destroy(thpool);
-	
-	return 0;
-}
+ #include <stdio.h>
+ #include <pthread.h>
+ #include <stdint.h>
+ #include <stdlib.h>
+ #include "thpool.h"
+ 
+ typedef struct
+ {
+   int id;
+ } workload;
+ 
+ void task(void *arg){
+	 printf("Thread #%u working on %d\n", (int)pthread_self(), (int) arg);
+ }
+ 
+ void task_via_struct(void *arg){
+	 workload wl = *((workload *)arg);
+	 printf("Thread #%u working on %d\n", (int)pthread_self(), wl.id);
+ }
+ 
+ int main(){
+	 
+	 puts("Making threadpool with 4 threads");
+	 threadpool thpool = thpool_init(4);
+ 
+	 puts("Adding 40 tasks to threadpool");
+	 int i;
+	 for (i=0; i<40; i++){
+		 thpool_add_work(thpool, task, (void*)(uintptr_t)i);
+	 };
+ 
+	 thpool_wait(thpool);
+ 
+	 puts("Adding 40 tasks to threadpool using struct");
+	 workload *instructions[40];
+	 for (i=0; i<40; i++){
+		 instructions[i]= malloc(sizeof(workload));
+		 instructions[i]->id=i;
+		 thpool_add_work(thpool, task_via_struct, instructions[i]);
+	 };
+ 
+	 thpool_wait(thpool);
+ 
+	 puts("Killing threadpool");
+	 thpool_destroy(thpool);
+ 
+	 for(i=0;i<40;i++){
+		 free(instructions[i]);
+	 }
+	 return 0;
+ }
